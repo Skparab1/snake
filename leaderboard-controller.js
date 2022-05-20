44e5dcbc3be654d067463fa880cb28921e63cd10
skparab1 <skparab1@gmail.com>
2022-05-19 20:28:09 -0700

    trying to debug leaderboard

--- leaderboard-controller.js
+++ leaderboard-controller.js
@@ -23,6 +23,8 @@
   var names = datanames.split('&=');
   var scores = datascores.split('&=');
 
+  console.log(names,scores);
+
   // names = ['Shubham','skparab1','poisonfy'];
   // scores = ['&=8&t36.997','&=7&t18.458','&=2&t38.261'];
   let points = [];
