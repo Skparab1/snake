b608748c67faac524f516da879d31a18abf29a19
Shubham <71990977+Skparab1@users.noreply.github.com>
2022-05-23 21:04:54 -0700

    insanely hard but good looking effect

--- leaderboard-controller.js
+++ leaderboard-controller.js
@@ -1,32 +1,32 @@
 (async () => {
-  const data1 = await fetch("./data.json").then(r => r.json());
+  // const data1 = await fetch("./data.json").then(r => r.json());
 
-  var datanames = data1.data[0];
-  var datascores = data1.data[1];
+  // var datanames = data1.data[0];
+  // var datascores = data1.data[1];
   
-  datanames = JSON.stringify(datanames);
-  datascores = JSON.stringify(datascores);
+  // datanames = JSON.stringify(datanames);
+  // datascores = JSON.stringify(datascores);
   
-  datanames = datanames.replace('{"name":"','');
-  datanames = datanames.replace('"}','');
-  datascores = datascores.replace('{"scores":"','');
-  datascores = datascores.replace('"}','');
+  // datanames = datanames.replace('{"name":"','');
+  // datanames = datanames.replace('"}','');
+  // datascores = datascores.replace('{"scores":"','');
+  // datascores = datascores.replace('"}','');
 
 
-  while (datanames.includes('+')){
-    datanames = datanames.replace('+',' ');
-  }
-  while (datascores.includes('+')){
-    datascores = datascores.replace('+',' ');
-  }
+  // while (datanames.includes('+')){
+  //   datanames = datanames.replace('+',' ');
+  // }
+  // while (datascores.includes('+')){
+  //   datascores = datascores.replace('+',' ');
+  // }
 
-  var names = datanames.split('&=');
-  var scores = datascores.split('&=');
+  // var names = datanames.split('&=');
+  // var scores = datascores.split('&=');
 
-  console.log(names,scores);
+  // console.log(names,scores);
 
-  // names = ['Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy'];
-  // scores = ['&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261'];
+  names = ['Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy','Shubham','skparab1','poisonfy'];
+  scores = ['&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261','&=8&t36.997','&=7&t18.458','&=2&t38.261'];
   let points = [];
   let times = [];
   let i = 0;
@@ -75,13 +75,36 @@
       if (ranker == 1){
         bgclr = "rgb(100,100,0)";
       } else if (ranker == 2){
-        bgclr = "rgb(50,50,50)";
-      } else if (ranker == 3){
-        bgclr = "rbg(50,25,0)";
+        bgclr = "rgb(150,150,150)";
+      } else if (ranker == 3 ){
+        bgclr = "rgb(75,75,75)";
       } else {
         bgclr = "black";
       }
-      if (names[i] == ' ' || names[i] == ''){
+
+      //255*3 = 765
+
+      if (ranker == 1){
+        display.innerHTML += `
+        <div style="background-color: `+bgclr+`" class="fullwidth">
+        <div id="d1" style="background: linear-gradient(to right, rgb(255,0,0), rgb(0,0,255));" class="left-container">
+          <h1 style="color: black">`+ranker+`</h1>
+        </div>
+        <div id="d2" style="background-color: `+bgclr+`;" class="right-container">
+          <h1 style="color: black">-</h1>
+        </div>
+        <div id="d3" style="background-color: `+bgclr+`" class="mid-container">
+          <h1 style="color: black">`+points[i]+`</h1>
+        </div>
+        <div id="d4" style="background-color: `+bgclr+`" class="mid-container">
+          <h1 style="color: black">`+times[i]+`</h1>
+        </div>
+        <div id="d5" style="background-color: `+bgclr+`" class="center-container">
+          <h1 style="color: black">`+rankscores[i]+`</h1>
+        </div>
+      </div>
+      <br>`;
+      } else if (names[i] == ' ' || names[i] == ''){
         display.innerHTML += `
         <div style="background-color: `+bgclr+`" class="fullwidth">
         <div style="background-color: `+bgclr+`" class="left-container">
@@ -153,5 +176,30 @@
   // <br>`;
   //   i += 1;
   // }
+
+  const sleep = ms => new Promise(res => setTimeout(res, ms));
+  (async () => {
+    let clr = -765;
+    let dir = 'left';
+    while (true){
+      let d = document.getElementById("d1");
+      d.style.background = "linear-gradient(to right, rgb("+Math.abs(255-clr)+","+Math.abs(510-clr)+","+(Math.abs(765-clr)+Math.abs(0-clr))+"), rgb("+Math.abs(255-(clr+153))+","+Math.abs(510-(clr+153))+","+(Math.abs(765-(clr+153))+Math.abs(-255-(clr+153)))+")";
+      d = document.getElementById("d2");
+      d.style.background = "linear-gradient(to right, rgb("+Math.abs(255-(clr+153))+","+Math.abs(510-(clr+153))+","+(Math.abs(765-(clr+153))+Math.abs(0-(clr+153)))+"), rgb("+Math.abs(255-(clr+306))+","+Math.abs(510-(clr+306))+","+(Math.abs(765-(clr+306))+Math.abs(-255-(clr+306)))+")";
+      d = document.getElementById("d3");
+      d.style.background = "linear-gradient(to right, rgb("+Math.abs(255-(clr+306))+","+Math.abs(510-(clr+306))+","+(Math.abs(765-(clr+306))+Math.abs(0-(clr+306)))+"), rgb("+Math.abs(255-(clr+459))+","+Math.abs(510-(clr+459))+","+(Math.abs(765-(clr+459))+Math.abs(-255-(clr+459)))+")";
+      d = document.getElementById("d4");
+      d.style.background = "linear-gradient(to right, rgb("+Math.abs(255-(clr+459))+","+Math.abs(510-(clr+459))+","+(Math.abs(765-(clr+459))+Math.abs(0-(clr+459)))+"), rgb("+Math.abs(255-(clr+612))+","+Math.abs(510-(clr+612))+","+(Math.abs(765-(clr+612))+Math.abs(-255-(clr+612)))+")";
+      d = document.getElementById("d5");
+      d.style.background = "linear-gradient(to right, rgb("+Math.abs(255-(clr+612))+","+Math.abs(510-(clr+612))+","+(Math.abs(765-(clr+612))+Math.abs(0-(clr+612)))+"), rgb("+Math.abs(255-(clr+765))+","+Math.abs(510-(clr+765))+","+(Math.abs(765-(clr+765))+Math.abs(-255-(clr+765)))+")";
+
+
+      if (clr >= 765){
+        clr = -765;
+      }
+      clr += 0.5;
+      await sleep(2);
+    }
+  })();
 
 })();
